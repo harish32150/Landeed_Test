fe7997910d9348e4fd6d4a6903644e4d59903e0c
Harish <harish.kumar@flobiz.in>
2024-01-30 23:39:06 +0530

    TimerInputComponent fixes

--- app/src/main/java/com/harishdevs/landeed_itest/ui/component/TimerComponent.kt
+++ app/src/main/java/com/harishdevs/landeed_itest/ui/component/TimerComponent.kt
@@ -1,23 +1,41 @@
 package com.harishdevs.landeed_itest.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
-import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 @Composable
 @Preview
@@ -35,6 +53,18 @@ fun TimerComponent(viewModel: TimerViewModel = androidx.lifecycle.viewmodel.comp
     }
 
     Box(modifier = Modifier.fillMaxSize()) {
+
+        LazyColumn(modifier = Modifier.padding(top = 24.dp, bottom = 16.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+            items(items = timerListState, key = { item: TimerInfo -> item.id }) {
+                TimerItemComponent(it, viewModel::removeTimer)
+            }
+            if (timerListState.isEmpty()) {
+                item {
+                    Text(text = "No Timers added, start by adding one.", style = MaterialTheme.typography.bodyLarge, modifier = Modifier.fillMaxWidth().padding(top = 64.dp), textAlign = TextAlign.Center)
+                }
+            }
+        }
+
         FloatingActionButton(
             onClick = { addTimerInputState = true },
             Modifier
@@ -42,9 +72,61 @@ fun TimerComponent(viewModel: TimerViewModel = androidx.lifecycle.viewmodel.comp
                 .padding(end = 24.dp, bottom = 48.dp)
         ) {
 
-
-
             Icon(imageVector = Icons.Outlined.Add, contentDescription = null)
         }
     }
+}
+
+@Composable
+private fun TimerItemComponent(timerInfo: TimerInfo = TimerInfo(0, 2, 30), onRemove: (TimerInfo) -> Unit) {
+    val ticker: Long by timerInfo.timerFlow.collectAsState(initial = 0)
+  /*  var ticker by remember {
+        mutableLongStateOf(timerInfo.endTime - System.currentTimeMillis())
+    }*/
+
+    Card(
+        modifier = Modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.secondary,
+                shape = MaterialTheme.shapes.medium
+            )
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = if (ticker == 0L) "Timed Out" else "Timer Running")
+
+            Text(text = "${ticker.div(1000L)} seconds Left")
+
+            IconButton(
+                onClick = { onRemove(timerInfo) },
+                modifier = Modifier.background(
+                    color = MaterialTheme.colorScheme.error.copy(alpha = 0.1f),
+                    shape = CircleShape
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Delete,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+    }
+
+//    LaunchedEffect(key1 = ticker, block = {
+//        while (timerInfo.endTime > System.currentTimeMillis()) {
+//            ticker = timerInfo.endTime - System.currentTimeMillis()
+//            delay(1000)
+//        }
+//    })
 }
--- app/src/main/java/com/harishdevs/landeed_itest/ui/component/TimerInputComponent.kt
+++ app/src/main/java/com/harishdevs/landeed_itest/ui/component/TimerInputComponent.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -17,19 +18,22 @@ import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 
 @Composable
 fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit) {
 
-    var hours by remember { mutableIntStateOf(0) }
-    var minutes by remember { mutableIntStateOf(0) }
-    var seconds by remember { mutableIntStateOf(0) }
+    var hours by remember { mutableStateOf(TextFieldValue("00")) }
+    var minutes by remember { mutableStateOf(TextFieldValue("00")) }
+    var seconds by remember { mutableStateOf(TextFieldValue("00")) }
 
     Dialog(onDismissRequest = onDismiss) {
         Card(
@@ -45,7 +49,7 @@ fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit)
                 Text(text = "Enter Time", style = MaterialTheme.typography.titleSmall)
 
                 Row(modifier = Modifier.padding(top = 16.dp)) {
-                    TimerInputField(label = "Hours", onValueChange = { hours = it })
+                    TimerInputField(label = "Hours", value = hours, onValueChange = { hours = it })
 
                     Text(
                         text = ":",
@@ -53,7 +57,7 @@ fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit)
                         modifier = Modifier.padding(horizontal = 8.dp)
                     )
 
-                    TimerInputField(label = "Minutes", onValueChange = { minutes = it })
+                    TimerInputField(label = "Minutes", value = minutes, onValueChange = { minutes = it })
 
                     Text(
                         text = ":",
@@ -61,7 +65,7 @@ fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit)
                         modifier = Modifier.padding(horizontal = 8.dp)
                     )
 
-                    TimerInputField(label = "Seconds", onValueChange = { seconds = it })
+                    TimerInputField(label = "Seconds", value = seconds, onValueChange = { seconds = it })
                 }
 
                 Row(
@@ -74,7 +78,10 @@ fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit)
                         Text(text = "CANCEL")
                     }
 
-                    TextButton(onClick = { /*TODO*/ }) {
+                    TextButton(onClick = {
+                        /* validate if required */
+                        onStart(hours.text.toInt(), minutes.text.toInt(), seconds.text.toInt())
+                    }) {
                         Text(text = "START")
                     }
                 }
@@ -85,7 +92,8 @@ fun TimerInputComponent(onDismiss: () -> Unit, onStart: (Int, Int, Int) -> Unit)
 
 @Composable
 private fun TimerInputField(
-    onValueChange: (Int) -> Unit,
+    onValueChange: (TextFieldValue) -> Unit,
+    value: TextFieldValue,
     label: String,
 ) {
     Column(
@@ -93,8 +101,8 @@ private fun TimerInputField(
         modifier = Modifier.wrapContentSize()
     ) {
         BasicTextField(
-            value = "00",
-            onValueChange = { onValueChange.invoke(it.toInt()) },
+            value = value,
+            onValueChange = { onValueChange.invoke(it) },
             textStyle = MaterialTheme.typography.displaySmall,
             singleLine = true,
             maxLines = 1,
@@ -105,7 +113,8 @@ private fun TimerInputField(
                     color = MaterialTheme.colorScheme.secondary,
                     shape = MaterialTheme.shapes.medium
                 )
-                .padding(horizontal = 8.dp, vertical = 4.dp)
+                .padding(horizontal = 8.dp, vertical = 4.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number)
         )
 
         Text(text = label, style = MaterialTheme.typography.bodyMedium)
